7b7fb77b1190d871e9cdb737fdd04e21f9640c14
Peer Jüttner <peer@hotmail.ch>
2018-08-17 22:00:13 +0200

    Âdd ability to add new entries (passing state from child component to parent)

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ class App extends Component {
     super(props);
     this.state = {
       entries: [
-        {id:1,date:"17.08.2018",time:"14:02",duration:"28"},
+        {id:1,date:"17.08.2017",time:"14:02",duration:"28"},
         {id:2,date:"15.08.2018",time:"18:56",duration:"25"},
         {id:3,date:"03.08.2018",time:"20:12",duration:"31"},
-      ]
+      ],
+      nextId: 4,
     };
+    this.handleNewListEntry = this.handleNewListEntry.bind(this);
   }
   render() {
 
@@ -22,11 +24,18 @@ class App extends Component {
           <h1 className="App-title">Welcome to the swim logger</h1>
         </header>
         <h3>Add a Swim:</h3>
-        <TableControls></TableControls>
+        <TableControls onItemSubmitted={this.handleNewListEntry}></TableControls>
         <h3>List of logged swimms:</h3>
         <Table tableData={this.state.entries} ></Table>
       </div>
     );
+  }
+  handleNewListEntry(event){
+   
+    //event.id = this.state.
+    this.setState({entries:[...this.state.entries, {id:this.state.nextId,date:event.date,time:event.time,duration:event.duration}]});
+    //this.setState({nextId:this.state.nextId+1});
+
   }
 }
 class TableControls extends Component {
@@ -46,8 +55,8 @@ class TableControls extends Component {
       <div id="controlContainer">
       <form onSubmit={this.handleSubmit}>
       <label>Date of Swim:<input name="date" type="text" value={this.state.value} onChange={this.handleInputChange}></input></label>
-      <label>Date of Swim:<input name="time" type="text" value={this.state.value} onChange={this.handleInputChange}></input></label>
-      <label>Date of Swim:<input name="duration" type="text" value={this.state.value} onChange={this.handleInputChange}></input></label>
+      <label>Time of Swim:<input name="time" type="text" value={this.state.value} onChange={this.handleInputChange}></input></label>
+      <label>Duration of Swim:<input name="duration" type="text" value={this.state.value} onChange={this.handleInputChange}></input></label>
       <input type="submit" value="Submit"></input>
       </form>
       </div>
@@ -55,7 +64,7 @@ class TableControls extends Component {
     )
   }
   handleSubmit(event){
-    alert()
+    this.props.onItemSubmitted(this.state);
     event.preventDefault();
   }
   handleInputChange(event){
@@ -94,5 +103,6 @@ class Table extends Component {
    );
   }
 }
+
  
 export default App;
