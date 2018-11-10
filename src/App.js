e64188bb6086d9e9ec197a3790f464df7ed070f1
Peer Jüttner <peer@hotmail.ch>
2018-11-10 20:31:10 +0100

    add firebase save handling code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import * as firebase from 'firebase';
 import firebaseConfig from './firebase.json';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 
 class App extends Component {
   constructor(props){
@@ -11,52 +13,75 @@ class App extends Component {
 
     this.state = {
       entries: [
-        {id:1,date:"17.08.2017",time:"14:02",duration:"28"},
-        {id:2,date:"15.08.2018",time:"18:56",duration:"25"},
-        {id:3,date:"03.08.2018",time:"20:12",duration:"31"},
-      ],
-      nextId: 4,
+        {id:1,date:"17.08.2017",time:"14:02",duration:"28",isInStore: false},
+        {id:2,date:"15.08.2018",time:"18:56",duration:"25",isInStore: false},
+        {id:3,date:"03.08.2018",time:"20:12",duration:"31",isInStore: false},
+      ], nextId : 0
     };
     this.handleNewListEntry = this.handleNewListEntry.bind(this);
     this.handlleRemoveListEntry = this.handlleRemoveListEntry.bind(this);
+    this.handleSave = this.handleSave.bind(this);
   }
   render() {
 
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to the swim logger</h1>
         </header>
         <h3>Add a Swim:</h3>
         <TableControls onItemSubmitted={this.handleNewListEntry}></TableControls>
         <h3>List of logged swimms:</h3>
-        <Table tableData={this.state.entries} handleRemoveEntry={this.handlleRemoveListEntry} ></Table>
+        <Table tableData={this.state.entries} handleRemoveEntry={this.handlleRemoveListEntry} handleSave={this.handleSave} ></Table>
       </div>
     );
   }
   handleNewListEntry(event){
-    this.setState({entries:[...this.state.entries, {id:this.state.nextId,date:event.date,time:event.time,duration:event.duration}]});
+    let db = firebase.firestore();
+    let id = db.collection("swims").doc().key;
+    this.setState({entries:[...this.state.entries, {id:id,date:event.date,time:event.time,duration:event.duration,isInStore: false}]});
+    this.handleSave(event);
   }
   handlleRemoveListEntry(rid,event){
     const newListEntries = this.state.entries.filter(entries => {return entries.id !== rid});
     this.setState({entries: [...newListEntries]})
   }
-
-  componentDidMount(){
+  handleSave(event){
+    event.preventDefault();
+    console.log("called handleSave");
+    let db = firebase.firestore();
+    db.collection("swims").doc();
+    this.state.entries.forEach(e => {
+      if (!e.isInStore){
+        db.collection("swims").add({
+          date: e.date,
+          duration: e.duration,
+          time: e.time
+      }).then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+      }
+    });
+    this.readFirebaseData();
+  }
+  readFirebaseData(){
     let newArr = null;
     var db = firebase.firestore();
     db.collection("swims").get().then((querySnapshot) => { 
       newArr = querySnapshot.docs.map(function(doc){
-        return {id : doc.id,date: doc.data().date,time: doc.data().time,duration: doc.data().duration};
+        return {id : doc.id,date: doc.data().date,time: doc.data().time,duration: doc.data().duration,isInStore: true};
       });
-      console.log(newArr);
       this.setState({
         entries:newArr
       });
-      
-
   });
+  }
+
+  componentDidMount(){
+    this.readFirebaseData();
   }
 }
 class TableControls extends Component {
@@ -69,13 +94,14 @@ class TableControls extends Component {
   };
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+
   }
   render(){
     return (
       <div id="controlContainer">
-      <form onSubmit={this.handleSubmit}>
-      <label>Date of Swim:<input name="date" type="date" value={this.state.value} onChange={this.handleInputChange}></input></label>
-      <label>Time of Swim:<input name="time" type="time" value={this.state.value} onChange={this.handleInputChange}></input></label>
+      <form onSubmit={this.handleSubmit} class="inputForm">
+      <label>Date of Swim:<input name="date" type="date" value={this.state.value} onChange={this.handleInputChange}></input></label><br></br>
+      <label>Time of Swim:<input name="time" type="time" value={this.state.value} onChange={this.handleInputChange}></input></label><br></br>
       <label>Duration of Swim (minutes):<input name="duration" type="number" value={this.state.value} onChange={this.handleInputChange}></input></label>
       <input type="submit" value="Submit"></input>
       </form>
@@ -99,6 +125,11 @@ class TableControls extends Component {
 }
 
 class Table extends Component {
+  constructor(props){
+    super(props)
+
+  }
+  
   render() {
     let listItems = null;
     if(this.props.tableData.length > 0){
@@ -127,12 +158,15 @@ class Table extends Component {
         </tr>
         {listItems}
         </tbody></table>
-        <button onClick={this.handleSaveTable}>Änderungen Speichern</button>
+        <button onClick={(e) => this.handleSaveTable(e)}>Änderungen Speichern</button>
     </div>
    );
   }
   handleRemoveEntry(id,event){
     this.props.handleRemoveEntry(id,event);
+  }
+  handleSaveTable(event){
+    this.props.handleSave(event);
   }
 }
 
